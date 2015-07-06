b912be04d6b43c0c016762d99a9578b730798aa9
Vanderson Lopes <vanderson139@hotmail.com>
2015-07-06 14:11:30 -0300

    Added Twig Library

--- upload/system/engine/loader.php
+++ upload/system/engine/loader.php
@@ -1,79 +1,118 @@
 <?php
 final class Loader {
-	private $registry;
+    private $registry;
 
-	public function __construct($registry) {
-		$this->registry = $registry;
-	}
+    public function __construct($registry) {
+        $this->registry = $registry;
+    }
 
-	public function controller($route, $args = array()) {
-		$action = new Action($route, $args);
+    public function controller($route, $args = array()) {
+        $action = new Action($route, $args);
 
-		return $action->execute($this->registry);
-	}
+        return $action->execute($this->registry);
+    }
 
-	public function model($model) {
-		$file = DIR_APPLICATION . 'model/' . $model . '.php';
-		$class = 'Model' . preg_replace('/[^a-zA-Z0-9]/', '', $model);
+    public function model($model) {
+        $file = DIR_APPLICATION . 'model/' . $model . '.php';
+        $class = 'Model' . preg_replace('/[^a-zA-Z0-9]/', '', $model);
 
-		if (file_exists($file)) {
-			include_once($file);
+        if (file_exists($file)) {
+            include_once($file);
 
-			$this->registry->set('model_' . str_replace('/', '_', $model), new $class($this->registry));
-		} else {
-			trigger_error('Error: Could not load model ' . $file . '!');
-			exit();
-		}
-	}
+            $this->registry->set('model_' . str_replace('/', '_', $model), new $class($this->registry));
+        } else {
+            trigger_error('Error: Could not load model ' . $file . '!');
+            exit();
+        }
+    }
 
-	public function view($template, $data = array()) {
-		$file = DIR_TEMPLATE . $template;
+    public function view($template, $data = array()) {
+        $file = DIR_TEMPLATE . $template;
 
-		if (file_exists($file)) {
-			extract($data);
+        if (file_exists($file)) {
+            if( ! $this->registry->get('twig')) {
 
-			ob_start();
+                $this->library('twig');
 
-			require($file);
+                Twig_Autoloader::register();
 
-			$output = ob_get_contents();
+                if($this->registry->get('config') && is_dir(DIR_TEMPLATE . $this->registry->get('config')->get('config_template') . '/template')) {
+                    $paths[] = DIR_TEMPLATE . $this->registry->get('config')->get('config_template') . '/template';
+                }
 
-			ob_end_clean();
+                if(is_dir(DIR_TEMPLATE . 'default/template')) {
+                    $paths[] = DIR_TEMPLATE . 'default/template';
+                }
 
-			return $output;
-		} else {
-			trigger_error('Error: Could not load template ' . $file . '!');
-			exit();
-		}
-	}
+                $paths[] = DIR_TEMPLATE;
 
-	public function library($library) {
-		$file = DIR_SYSTEM . 'library/' . $library . '.php';
+                $loader = new Twig_Loader_Chain(array(new Twig_Loader_Filesystem($paths), new Twig_Loader_String()));
 
-		if (file_exists($file)) {
-			include_once($file);
-		} else {
-			trigger_error('Error: Could not load library ' . $file . '!');
-			exit();
-		}
-	}
+                $cache = false;
 
-	public function helper($helper) {
-		$file = DIR_SYSTEM . 'helper/' . $helper . '.php';
+                if(defined('TWIG_CACHE')) {
+                    $cache = TWIG_CACHE;
+                }
 
-		if (file_exists($file)) {
-			include_once($file);
-		} else {
-			trigger_error('Error: Could not load helper ' . $file . '!');
-			exit();
-		}
-	}
+                $twig = new Twig_Environment($loader,array(
+                    'autoescape' => false,
+                    'cache' => $cache,
+                    'debug' => true
+                ));
 
-	public function config($config) {
-		$this->registry->get('config')->load($config);
-	}
+                $twig->addExtension(new Twig_Extension_Debug());
+                $twig->addExtension(new Twig_Extension_Opencart($this->registry));
 
-	public function language($language) {
-		return $this->registry->get('language')->load($language);
-	}
+                $this->registry->set('twig', $twig);
+            }
+
+            extract($data);
+            ob_start();
+
+            // First Step - Render Twig Native Templates
+            $output = $this->registry->get('twig')->render($template, $data);
+
+            // Second Step - IF template has PHP Syntax, then execute
+            eval(' ?>' . $output);
+
+            $output = ob_get_contents();
+
+            ob_end_clean();
+
+            return $output;
+        } else {
+            trigger_error('Error: Could not load template ' . $file . '!');
+            exit();
+        }
+    }
+
+    public function library($library) {
+        $file = DIR_SYSTEM . 'library/' . $library . '.php';
+
+        if (file_exists($file)) {
+            include_once($file);
+        } else {
+            trigger_error('Error: Could not load library ' . $file . '!');
+            exit();
+        }
+    }
+
+    public function helper($helper) {
+        $file = DIR_SYSTEM . 'helper/' . $helper . '.php';
+
+        if (file_exists($file)) {
+            include_once($file);
+        } else {
+            trigger_error('Error: Could not load helper ' . $file . '!');
+            exit();
+        }
+    }
+
+    public function config($config) {
+        $this->registry->get('config')->load($config);
+    }
+
+    public function language($language) {
+        return $this->registry->get('language')->load($language);
+    }
 }
